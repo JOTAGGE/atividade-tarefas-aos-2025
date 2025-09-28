75dd328894c8196787be663a074c13c64e79d958
JOTAGGE <108813623+JOTAGGE@users.noreply.github.com>
2025-09-28 10:06:14 +0000

    porta 3000

--- api/app.js
+++ api/app.js
@@ -6,12 +6,18 @@ const tarefasRoutes = require('./routes/tarefasRoutes');
 
 // 1. Ignição do Servidor
 const app = express();
+const PORT = process.env.PORT || 3000; // Define a porta
 
 // 2. Middleware Essencial
 app.use(express.json());
 
 // 3. Conectar as Linhas de Comunicação
 app.use('/api/tarefas', tarefasRoutes);
+
+// 4. Ligar o Motor e Ouvir por Ordens
+app.listen(PORT, () => {
+  console.log(`Servidor de combate operacional na porta ${PORT}`);
+});
 
 // 5. Exportar o Quartel-General para a Vercel
 module.exports = app;
